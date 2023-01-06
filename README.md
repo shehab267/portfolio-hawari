9dd1280a43eff91cf9c24a1a972ed432afcbc40e
shehab267 <shehabelhawary267@gmail.com>
2023-01-06 10:25:28 +0200

    up to finished

--- README.md
+++ README.md
@@ -1,7 +1,33 @@
 # Hawari Portfolio Website
 
+### Features:
+
 - Responsive Personal Portfolio Website Using HTML CSS & JavaScript
 - Smooth scrolling in each section.
-- Includes a light and dark mode.
+- Includes a light and dark mode, Using localStorage to save the value
 - Developed first with the Mobile First methodology, then for desktop.
 - Compatible with all mobile devices and with a beautiful and pleasant user interface.
+
+#### [Live Preview of Website](https://shehab267.github.io/Hawari-Portfolio-Website/)
+
+feel free to comment on the code. If you like it give it a star:star:
+
+### Files have been used
+
+- ##### [MixItUp 3.3.1](https://www.kunkalabs.com/mixitup/)
+- ##### [Swiper 7.4.1](https://swiperjs.com)
+- ##### [ScrollReveal 4.0.9](https://scrollrevealjs.org/)
+
+- ##### Poppins ['google-font'](https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap)
+
+### Acknowledgement
+
+- [MDN](https://developer.mozilla.org/en-US/ " Mozilla Developer Network")
+- [Code With Pankaj](https://www.youtube.com/c/CodeWithPankaj1?sub_confirmation=1)
+
+- [Stack Overflow](https://stackoverflow.com/ "Stack Overflow")
+- [Bedimcode](https://www.youtube.com/c/Bedimcode)
+
+### License & Copyright
+
+© Shehab Alhawary
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,56 +1,164 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
 const navMenu = document.getElementById("nav-menu"),
-    navToggle = document.getElementById("nav-toggle"),
-    navClose = document.getElementById("nav-close");
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
 if (navToggle) {
-    navToggle.addEventListener("click", () => {
-        navMenu.classList.add("show-menu");
-    });
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
 }
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
 if (navClose) {
-    navClose.addEventListener("click", () => {
-        navMenu.classList.remove("show-menu");
-    });
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
 }
 
 /*==================== REMOVE MENU MOBILE ====================*/
 const navLink = document.querySelectorAll(".nav__link");
 
 function linkAction() {
-    const navMenu = document.getElementById("nav-menu");
-    // when we click on each nav__link, we remove the show menu class
-    navMenu.classList.remove("show-menu");
+  const navMenu = document.getElementById("nav-menu");
+  // when we click on each nav__link, we remove the show menu class
+  navMenu.classList.remove("show-menu");
 }
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
-/*==================== ACCORDION SKILLS ====================*/
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader() {
+  const header = document.getElementById("header");
+  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 50) header.classList.add("scroll-header");
+  else header.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
 
+/*=============== SERVICES MODAL ===============*/
+const modalViews = document.querySelectorAll(".services__modal"),
+  modalBtns = document.querySelectorAll(".services__button"),
+  modalClose = document.querySelectorAll(".services__modal-close");
 
-/*==================== QUALIFICATION TABS ====================*/
+let modal = function (modalClick) {
+  modalViews[modalClick].classList.add("active-modal");
+};
 
+modalBtns.forEach((mb, i) => {
+  mb.addEventListener("click", () => {
+    modal(i);
+  });
+});
 
-/*==================== SERVICES MODAL ====================*/
+modalClose.forEach((mc) => {
+  mc.addEventListener("click", () => {
+    modalViews.forEach((mv) => {
+      mv.classList.remove("active-modal");
+    });
+  });
+});
 
+/*=============== MIXITUP FILTER PORTFOLIO ===============*/
+let mixerPortfolio = mixitup(".work__container", {
+  selectors: {
+    target: ".work__card",
+  },
+  animation: {
+    duration: 300,
+  },
+});
 
-/*==================== PORTFOLIO SWIPER  ====================*/
+/* Link active work */
+const linkWork = document.querySelectorAll(".work__item");
 
+function activeWork() {
+  linkWork.forEach((l) => l.classList.remove("active-work"));
+  this.classList.add("active-work");
+}
 
-/*==================== TESTIMONIAL ====================*/
+linkWork.forEach((l) => l.addEventListener("click", activeWork));
 
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
 
-/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const scrollActive = () => {
+  const scrollY = window.pageYOffset;
 
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionsClass = document.querySelector(
+        ".nav__menu a[href*=" + sectionId + "]"
+      );
 
-/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
 
+/*=============== LIGHT DARK THEME ===============*/
+const themeButton = document.getElementById("theme-button");
+const lightTheme = "light-theme";
+const iconTheme = "bx-sun";
 
-/*==================== SHOW SCROLL UP ====================*/ 
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
 
+// We obtain the current theme that the interface has by validating the light-theme class
+const getCurrentTheme = () =>
+  document.body.classList.contains(lightTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "bx bx-moon" : "bx bx-sun";
 
-/*==================== DARK LIGHT THEME ====================*/ 
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    lightTheme
+  );
+  themeButton.classList[selectedIcon === "bx bx-moon" ? "add" : "remove"](
+    iconTheme
+  );
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener("click", () => {
+  // Add or remove the dark / icon theme
+  document.body.classList.toggle(lightTheme);
+  themeButton.classList.toggle(iconTheme);
+  // We save the theme and the current icon that the user chose
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  deplay: 400,
+  // reset: true
+});
+
+sr.reveal(`.home__data`);
+sr.reveal(`.home__handle`, { deplay: 700 });
+sr.reveal(`.home__social, .home__scroll`, { deplay: 900, origin: "bottom" });
+sr.reveal(``);
+
+/*==================== SHOW SCROLL UP ====================*/
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
+}
+
+window.addEventListener("scroll", scrollUp);
